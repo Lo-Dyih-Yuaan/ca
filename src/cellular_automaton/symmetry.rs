3ad3999df968c067752f1e1cb7a3140c113c3715
Lo-Dyih-Yuaan <lzy1999891@163.com>
2020-06-04 20:51:13 +0800

    Update symmetry.rs
    将专用于life-like non-totalistic规则的宏拓展至通用的$D_4$群对称。

--- src/cellular_automaton/symmetry.rs
+++ src/cellular_automaton/symmetry.rs
@@ -1,4 +1,5 @@
-macro_rules! non_totalistic {
+//$D_4$群对称
+macro_rules! d4_symmetry {
 	(@ ;
 	 $p11:pat,$p12:pat,$p13:pat,$p21:pat,$p23:pat,$p31:pat,$p32:pat,$p33:pat) => 
 		{($p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33)};
@@ -6,7 +7,7 @@ macro_rules! non_totalistic {
 	 $p11:pat, $p12:pat, $p13:pat,
 	 $p21:pat,           $p23:pat,
 	 $p31:pat, $p32:pat, $p33:pat) => {
-		non_totalistic!(@ $($i)*;
+		d4_symmetry!(@ $($i)*;
 			$p31, $p21, $p11,
 			$p32,       $p12,
 			$p33, $p23, $p13)
@@ -15,24 +16,33 @@ macro_rules! non_totalistic {
 	 $p11:pat, $p12:pat, $p13:pat,
 	 $p21:pat,           $p23:pat,
 	 $p31:pat, $p32:pat, $p33:pat) => {
-		non_totalistic!(@ $($i)*;
+		d4_symmetry!(@ $($i)*;
 			$p31, $p32, $p33,
 			$p21,       $p23,
 			$p11, $p12, $p13)
 	};
+	($t:ty;$p11:pat,$p12:pat,$p13:pat,$p21:pat,$p23:pat,$p31:pat,$p32:pat,$p33:pat) => {
+		Box::new(
+			|c11:&$t,c12:&$t,c13:&$t,c21:&$t,c23:&$t,c31:&$t,c32:&$t,c33:&$t|
+			d4_symmetry!{
+				$p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33,
+				(c11,c12,c13,c21,c23,c31,c32,c33)
+			}
+		) as Box<dyn Fn(&$t,&$t,&$t,&$t,&$t,&$t,&$t,&$t) -> bool>
+	};
 	($p11:pat,$p12:pat,$p13:pat,$p21:pat,$p23:pat,$p31:pat,$p32:pat,$p33:pat, $x:expr) => {
 		matches!{
 			$x,
-			non_totalistic!(@; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
-			non_totalistic!(@rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
-			non_totalistic!(@rotate rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
-			non_totalistic!(@rotate rotate rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
-			non_totalistic!(@flip; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
-			non_totalistic!(@flip rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
-			non_totalistic!(@flip rotate rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
-			non_totalistic!(@flip rotate rotate rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33)
+			d4_symmetry!(@; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
+			d4_symmetry!(@rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
+			d4_symmetry!(@rotate rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
+			d4_symmetry!(@rotate rotate rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
+			d4_symmetry!(@flip; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
+			d4_symmetry!(@flip rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
+			d4_symmetry!(@flip rotate rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33) |
+			d4_symmetry!(@flip rotate rotate rotate; $p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33)
 		}
-	}
+	};
 }
 
 /*macro_rules! non_totalistic_v {
@@ -75,74 +85,64 @@ macro_rules! non_totalistic {
 /*使用该宏可能引起大量警告，建议使用`#[allow(unreachable_patterns)]`阻止*/
 macro_rules! non_totalistic_closure {
 	/*宏内部使用*/
-	($t:ty;$p11:pat,$p12:pat,$p13:pat,$p21:pat,$p23:pat,$p31:pat,$p32:pat,$p33:pat) => {
-		Box::new(
-			|c11:&$t,c12:&$t,c13:&$t,c21:&$t,c23:&$t,c31:&$t,c32:&$t,c33:&$t|
-			non_totalistic!{
-				$p11,$p12,$p13,$p21,$p23,$p31,$p32,$p33,
-				(c11,c12,c13,c21,c23,c31,c32,c33)
-			}
-		) as Box<dyn Fn(&$t,&$t,&$t,&$t,&$t,&$t,&$t,&$t) -> bool>
-	};
-	/*宏内部使用*/
 	($t:ty; $p:pat, $n:expr, $c:expr) => {
 		match ($n, $c) {
 			//1
-			(1, 'c') => non_totalistic_closure!($t;$p,_,_,_,_,_,_,_),
-			(1, 'e') => non_totalistic_closure!($t;_,$p,_,_,_,_,_,_),
+			(1, 'c') => d4_symmetry!($t;$p,_,_,_,_,_,_,_),
+			(1, 'e') => d4_symmetry!($t;_,$p,_,_,_,_,_,_),
 			//2
-			(2, 'c') => non_totalistic_closure!($t;$p,_,$p,_,_,_,_,_),
-			(2, 'e') => non_totalistic_closure!($t;_,$p,_,$p,_,_,_,_),
-			(2, 'k') => non_totalistic_closure!($t;$p,_,_,_,$p,_,_,_),
-			(2, 'a') => non_totalistic_closure!($t;$p,$p,_,_,_,_,_,_),
-			(2, 'i') => non_totalistic_closure!($t;_,$p,_,_,_,_,$p,_),
-			(2, 'n') => non_totalistic_closure!($t;$p,_,_,_,_,_,_,$p),
+			(2, 'c') => d4_symmetry!($t;$p,_,$p,_,_,_,_,_),
+			(2, 'e') => d4_symmetry!($t;_,$p,_,$p,_,_,_,_),
+			(2, 'k') => d4_symmetry!($t;$p,_,_,_,$p,_,_,_),
+			(2, 'a') => d4_symmetry!($t;$p,$p,_,_,_,_,_,_),
+			(2, 'i') => d4_symmetry!($t;_,$p,_,_,_,_,$p,_),
+			(2, 'n') => d4_symmetry!($t;$p,_,_,_,_,_,_,$p),
 			//3
-			(3, 'c') => non_totalistic_closure!($t;$p,_,$p,_,_,$p,_,_),
-			(3, 'e') => non_totalistic_closure!($t;_,$p,_,$p,$p,_,_,_),
-			(3, 'k') => non_totalistic_closure!($t;$p,_,_,_,$p,_,$p,_),
-			(3, 'a') => non_totalistic_closure!($t;$p,$p,_,$p,_,_,_,_),
-			(3, 'i') => non_totalistic_closure!($t;$p,$p,$p,_,_,_,_,_),
-			(3, 'n') => non_totalistic_closure!($t;$p,_,$p,_,$p,_,_,_),
-			(3, 'y') => non_totalistic_closure!($t;$p,_,$p,_,_,_,$p,_),
-			(3, 'q') => non_totalistic_closure!($t;$p,$p,_,_,_,_,_,$p),
-			(3, 'j') => non_totalistic_closure!($t;$p,$p,_,_,$p,_,_,_),
-			(3, 'r') => non_totalistic_closure!($t;$p,$p,_,_,_,_,$p,_),
+			(3, 'c') => d4_symmetry!($t;$p,_,$p,_,_,$p,_,_),
+			(3, 'e') => d4_symmetry!($t;_,$p,_,$p,$p,_,_,_),
+			(3, 'k') => d4_symmetry!($t;$p,_,_,_,$p,_,$p,_),
+			(3, 'a') => d4_symmetry!($t;$p,$p,_,$p,_,_,_,_),
+			(3, 'i') => d4_symmetry!($t;$p,$p,$p,_,_,_,_,_),
+			(3, 'n') => d4_symmetry!($t;$p,_,$p,_,$p,_,_,_),
+			(3, 'y') => d4_symmetry!($t;$p,_,$p,_,_,_,$p,_),
+			(3, 'q') => d4_symmetry!($t;$p,$p,_,_,_,_,_,$p),
+			(3, 'j') => d4_symmetry!($t;$p,$p,_,_,$p,_,_,_),
+			(3, 'r') => d4_symmetry!($t;$p,$p,_,_,_,_,$p,_),
 			//4
-			(4, 'c') => non_totalistic_closure!($t;$p,_,$p,_,_,$p,_,$p),
-			(4, 'e') => non_totalistic_closure!($t;_,$p,_,$p,$p,_,$p,_),
-			(4, 'k') => non_totalistic_closure!($t;$p,_,$p,$p,_,_,$p,_),
-			(4, 'a') => non_totalistic_closure!($t;$p,$p,$p,_,$p,_,_,_),
-			(4, 'i') => non_totalistic_closure!($t;$p,_,$p,$p,$p,_,_,_),
-			(4, 'n') => non_totalistic_closure!($t;$p,$p,$p,_,_,$p,_,_),
-			(4, 'y') => non_totalistic_closure!($t;$p,_,$p,_,$p,$p,_,_),
-			(4, 'q') => non_totalistic_closure!($t;$p,$p,_,$p,_,_,_,$p),
-			(4, 'j') => non_totalistic_closure!($t;$p,$p,_,_,$p,_,$p,_),
-			(4, 'r') => non_totalistic_closure!($t;$p,$p,_,$p,$p,_,_,_),
-			(4, 't') => non_totalistic_closure!($t;$p,$p,$p,_,_,_,$p,_),
-			(4, 'w') => non_totalistic_closure!($t;$p,$p,_,_,$p,_,_,$p),
-			(4, 'z') => non_totalistic_closure!($t;$p,$p,_,_,_,_,$p,$p),
+			(4, 'c') => d4_symmetry!($t;$p,_,$p,_,_,$p,_,$p),
+			(4, 'e') => d4_symmetry!($t;_,$p,_,$p,$p,_,$p,_),
+			(4, 'k') => d4_symmetry!($t;$p,_,$p,$p,_,_,$p,_),
+			(4, 'a') => d4_symmetry!($t;$p,$p,$p,_,$p,_,_,_),
+			(4, 'i') => d4_symmetry!($t;$p,_,$p,$p,$p,_,_,_),
+			(4, 'n') => d4_symmetry!($t;$p,$p,$p,_,_,$p,_,_),
+			(4, 'y') => d4_symmetry!($t;$p,_,$p,_,$p,$p,_,_),
+			(4, 'q') => d4_symmetry!($t;$p,$p,_,$p,_,_,_,$p),
+			(4, 'j') => d4_symmetry!($t;$p,$p,_,_,$p,_,$p,_),
+			(4, 'r') => d4_symmetry!($t;$p,$p,_,$p,$p,_,_,_),
+			(4, 't') => d4_symmetry!($t;$p,$p,$p,_,_,_,$p,_),
+			(4, 'w') => d4_symmetry!($t;$p,$p,_,_,$p,_,_,$p),
+			(4, 'z') => d4_symmetry!($t;$p,$p,_,_,_,_,$p,$p),
 			//5
-			(5, 'c') => non_totalistic_closure!($t;_,$p,_,$p,$p,_,$p,$p),
-			(5, 'e') => non_totalistic_closure!($t;$p,_,$p,_,_,$p,$p,$p),
-			(5, 'k') => non_totalistic_closure!($t;_,$p,$p,$p,_,$p,_,$p),
-			(5, 'a') => non_totalistic_closure!($t;_,_,$p,_,$p,$p,$p,$p),
-			(5, 'i') => non_totalistic_closure!($t;_,_,_,$p,$p,$p,$p,$p),
-			(5, 'n') => non_totalistic_closure!($t;_,$p,_,$p,_,$p,$p,$p),
-			(5, 'y') => non_totalistic_closure!($t;_,$p,_,$p,$p,$p,_,$p),
-			(5, 'q') => non_totalistic_closure!($t;_,_,$p,$p,$p,$p,$p,_),
-			(5, 'j') => non_totalistic_closure!($t;_,_,$p,$p,_,$p,$p,$p),
-			(5, 'r') => non_totalistic_closure!($t;_,_,$p,$p,$p,$p,_,$p),
+			(5, 'c') => d4_symmetry!($t;_,$p,_,$p,$p,_,$p,$p),
+			(5, 'e') => d4_symmetry!($t;$p,_,$p,_,_,$p,$p,$p),
+			(5, 'k') => d4_symmetry!($t;_,$p,$p,$p,_,$p,_,$p),
+			(5, 'a') => d4_symmetry!($t;_,_,$p,_,$p,$p,$p,$p),
+			(5, 'i') => d4_symmetry!($t;_,_,_,$p,$p,$p,$p,$p),
+			(5, 'n') => d4_symmetry!($t;_,$p,_,$p,_,$p,$p,$p),
+			(5, 'y') => d4_symmetry!($t;_,$p,_,$p,$p,$p,_,$p),
+			(5, 'q') => d4_symmetry!($t;_,_,$p,$p,$p,$p,$p,_),
+			(5, 'j') => d4_symmetry!($t;_,_,$p,$p,_,$p,$p,$p),
+			(5, 'r') => d4_symmetry!($t;_,_,$p,$p,$p,$p,_,$p),
 			//6
-			(6, 'c') => non_totalistic_closure!($t;_,$p,_,$p,$p,$p,$p,$p),
-			(6, 'e') => non_totalistic_closure!($t;$p,_,$p,_,$p,$p,$p,$p),
-			(6, 'k') => non_totalistic_closure!($t;_,$p,$p,$p,_,$p,$p,$p),
-			(6, 'a') => non_totalistic_closure!($t;_,_,$p,$p,$p,$p,$p,$p),
-			(6, 'i') => non_totalistic_closure!($t;$p,_,$p,$p,$p,$p,_,$p),
-			(6, 'n') => non_totalistic_closure!($t;_,$p,$p,$p,$p,$p,$p,_),
+			(6, 'c') => d4_symmetry!($t;_,$p,_,$p,$p,$p,$p,$p),
+			(6, 'e') => d4_symmetry!($t;$p,_,$p,_,$p,$p,$p,$p),
+			(6, 'k') => d4_symmetry!($t;_,$p,$p,$p,_,$p,$p,$p),
+			(6, 'a') => d4_symmetry!($t;_,_,$p,$p,$p,$p,$p,$p),
+			(6, 'i') => d4_symmetry!($t;$p,_,$p,$p,$p,$p,_,$p),
+			(6, 'n') => d4_symmetry!($t;_,$p,$p,$p,$p,$p,$p,_),
 			//7
-			(7, 'c') => non_totalistic_closure!($t;_,$p,$p,$p,$p,$p,$p,$p),
-			(7, 'e') => non_totalistic_closure!($t;$p,_,$p,$p,$p,$p,$p,$p),
+			(7, 'c') => d4_symmetry!($t;_,$p,$p,$p,$p,$p,$p,$p),
+			(7, 'e') => d4_symmetry!($t;$p,_,$p,$p,$p,$p,$p,$p),
 			//其他
 			_ => unreachable!()
 		}
